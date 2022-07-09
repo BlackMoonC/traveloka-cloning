75afdcc02c1fa2a7aeaccb4ae9bc504897c35d23
BlackMoonC <51168038+BlackMoonC@users.noreply.github.com>
2022-07-09 10:27:24 +0700

    upload file

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        mulish : "'Mulish', sans-serif",
+      }
+      ,
+      colors: {
+        black : '#1B1B1B',
+        blue : '#1BA0E2',
+        'blue200' : '#1494C6',
+        'blue300' : '#0D7FCC',
+      }
+    },
+  },
+  plugins: [],
+}
